f03a7def1b04fe8a54ead196e5a4a3d8583ecf04
dianlujitao <dianlujitao@lineageos.org>
2017-11-19 11:17:04 +0800

    msm8974-common: Add prebuilt libprotobuf-cpp-lite from sdk29
    We'll be using it for our libwvdrmengine.so blob.
    
    Change-Id: I980a9e66cc0f99b5292210428c6ae9737e5b3969

--- common.mk
+++ common.mk
@@ -327,6 +327,10 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
+
 # Widevine DRM
 PRODUCT_PACKAGES += \
     libprotobuf_shim
