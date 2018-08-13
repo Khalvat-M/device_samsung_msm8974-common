7730d1ba2bf4831534858daed5e6ebdca8700ac1
brinlyau <uberlaggydarwin@gmail.com>
2018-08-14 00:16:55 +0200

    msm8974-common: remove languages-full.mk
     * full_base_telephony.mk already includes language pack
    
    Change-Id: I0e28542250d6cfe80b6649add486876780754ca3

--- common.mk
+++ common.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The CyanogenMod Project
+# Copyright (C) 2022 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
