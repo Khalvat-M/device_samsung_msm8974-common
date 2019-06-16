82e0036bbec61a335b38043449654db14d390920
Arne Coucheron <arco68@gmail.com>
2019-06-16 05:32:57 +0000

    move forgoted firmware links to vendor

--- Android.mk
+++ Android.mk
@@ -153,13 +153,13 @@ $(VENUS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 ALL_DEFAULT_INSTALLED_MODULES += $(VENUS_SYMLINKS)
 
 # Create links for audcal data files
-$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wcd9320; \
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/etc/firmware/wcd9320; \
 	ln -sf /data/misc/audio/wcd9320_anc.bin \
-		$(TARGET_OUT)/etc/firmware/wcd9320/wcd9320_anc.bin;\
+		$(TARGET_OUT_VENDOR)/etc/firmware/wcd9320/wcd9320_anc.bin;\
 	ln -sf /data/misc/audio/mbhc.bin \
-		$(TARGET_OUT)/etc/firmware/wcd9320/wcd9320_mbhc.bin; \
+		$(TARGET_OUT_VENDOR)/etc/firmware/wcd9320/wcd9320_mbhc.bin; \
 	ln -sf /data/misc/audio/wcd9320_mad_audio.bin \
-		$(TARGET_OUT)/etc/firmware/wcd9320/wcd9320_mad_audio.bin)
+		$(TARGET_OUT_VENDOR)/etc/firmware/wcd9320/wcd9320_mad_audio.bin)
 
 endif
 endif
