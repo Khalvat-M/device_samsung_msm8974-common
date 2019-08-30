fae73538e6303335ddf857d67fb30408d8d2f673
MHermit <mohammad0592003@yahoo.com>
2019-08-30 18:23:05 +0430

    Add BoringSSL compatibility library

--- klte.mk
+++ klte.mk
@@ -88,6 +88,7 @@ PRODUCT_PACKAGES += \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
     memtrack.msm8974 \
+    libboringssl-compat \
     liboverlay
 
 # Device uses high-density artwork where available
