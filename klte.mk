fef9f09a7db02624d1128f4509cd5de705c322a0
Khalvat-M <mmba313@gmail.com>
2020-03-27 00:28:10 +0430

    Build FlipFlap package

--- klte.mk
+++ klte.mk
@@ -173,6 +173,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 
+# FlipFlap
+PRODUCT_PACKAGES += \
+    FlipFlap
+
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
