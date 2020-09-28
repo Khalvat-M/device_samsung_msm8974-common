b922e361880c0f8cf2dd1f9d67c097fa583b8fea
Khalvat-M <mmba313@gmail.com>
2020-09-28 05:49:53 +0330

    Disable debug.sf.recomputecrop
    Signed-off-by: Mohammad(Khalvat-M)<mmba313@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -54,6 +54,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.renderer=opengl \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
+    debug.sf.recomputecrop=0 \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0
 
