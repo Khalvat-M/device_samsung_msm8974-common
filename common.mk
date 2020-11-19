af71e9777a913e71342a2ce211ed58bf7be4f0a0
Khalvat-M <mmba313@gmail.com>
2020-11-19 14:59:48 +0330

    msm8974-common: Revert "Exclude lineage-sdk and Snap overlays from RRO"

--- common.mk
+++ common.mk
@@ -23,9 +23,6 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay/lineage-sdk \
-    $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # AAPT Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
