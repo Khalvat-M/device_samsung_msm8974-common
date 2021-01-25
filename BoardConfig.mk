26783a9eaf2907ca0cba38187b4df6566f5fa4c6
Khalvat-M <mmba313@gmail.com>
2021-01-25 13:35:43 +0330

    msm8974-common: Disable serial console from cmd line

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,7 +83,7 @@ TARGET_SPECIFIC_HEADER_PATH += $(COMMON_PATH)/include
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 zcache.enabled=1 zcache.compressor=lz4
+BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 zcache.enabled=1 zcache.compressor=lz4
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
