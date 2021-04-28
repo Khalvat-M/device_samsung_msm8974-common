c14e5d03c7b68a10a8462245af511df05cbef7e1
Khalvat-M <mmba313@gmail.com>
2021-04-28 09:35:23 +0430

    msm8974-common: Update Camera package

--- common.mk
+++ common.mk
@@ -71,8 +71,11 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.5 \
     camera.device@1.0-impl \
+    camera.device@3.2-impl \
     camera.msm8974 \
     libshim_binder \
     libshim_camera \
