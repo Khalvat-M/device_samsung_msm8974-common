3ce776a035ea3b56108a50aaa226db4e61231246
sashascurtu <sasha.scurtu@gmail.com>
2021-06-30 20:07:51 +0530

    msm8974-common: Build power stats HAL and label it
    BatteryStatsService: Unable to load Power Hal or power.stats HAL
    
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

--- common.mk
+++ common.mk
@@ -220,7 +220,8 @@ PRODUCT_COPY_FILES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Privapp Whitelist
 PRODUCT_COPY_FILES += \
