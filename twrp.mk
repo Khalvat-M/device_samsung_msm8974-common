ad98699945e635296e9a91858b925a57d425e36c
Khalvat-M <mmba313@gmail.com>
2021-07-07 16:01:53 +0430

    msm8974-common: Add some TWRP flags

--- twrp.mk
+++ twrp.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,10 +14,17 @@
 
 TARGET_RECOVERY_DEVICE_DIRS += $(COMMON_PATH)/twrp
 
+ALLOW_MISSING_DEPENDENCIES := true
+BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_VARIANT := twrp
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/twrp/recovery/root/etc/twrp.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_EXCLUDE_BASH := true
+TW_EXCLUDE_TWRPAPP := true
+TW_EXCLUDE_TZDATA := true
 TW_DEFAULT_BRIGHTNESS := 162
 TW_HAS_DOWNLOAD_MODE := true
 TW_INCLUDE_CRYPTO := true
@@ -26,3 +33,4 @@ TW_MTP_DEVICE := "/dev/mtp_usb"
 TW_NO_EXFAT_FUSE := true
 TW_NO_REBOOT_BOOTLOADER := true
 TW_THEME := portrait_hdpi
+TW_USE_TOOLBOX := true
