f37ea2f5dd41ef0513a8d58b68c153366754f2c4
Khalvat-M <mmba313@gmail.com>
2022-02-21 19:15:24 +0330

    msm8974-common: Build Thermal Hal v1.0

--- common.mk
+++ common.mk
@@ -289,6 +289,10 @@ PRODUCT_PACKAGES += \
     TimeKeep
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8974.conf
 
