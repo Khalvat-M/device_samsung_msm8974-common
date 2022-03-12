c9d2f54b0ee286e51c16b39c7722038f017c1aeb
Khalvat-M <mmba313@gmail.com>
2022-03-12 18:24:06 +0330

    msm8974-common: TARGET_KERNEL_CLANG_COMPILE := false
    Needed after 
    https://github.com/LineageOS/android_vendor_lineage/commit/6d88852d2e602a92dde7bea849ff614f6a5165b1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,6 +83,7 @@ BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+TARGET_KERNEL_CLANG_COMPILE := false
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
