bb7e3a640f1083b720170fc9de99d7f95a5cb611
Khalvat-M <mmba313@gmail.com>
2022-05-17 15:13:51 +0430

    msm8974-common: back to commonized bcm2079x NFC

--- nfc/bcm2079x/Android.mk
+++ nfc/bcm2079x/Android.mk
@@ -1,45 +1,18 @@
-LOCAL_PATH := $(call my-dir)
+#
+# Copyright 2016 The CyanogenMod Project
+# Copyright 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-D_CFLAGS := \
-    -DANDROID -DBUILDCFG=1 \
-    -Wno-deprecated-register \
-    -Wno-unused-parameter \
-    -Wno-missing-field-initializers \
-    -Wno-unused-function \
-    -Wno-unused-variable \
-    -Wno-macro-redefined
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_VENDOR_MODULE := true
-
-LOCAL_MODULE := nfc_nci.$(TARGET_BOARD_PLATFORM)
-LOCAL_VINTF_FRAGMENTS := android.hardware.nfc@1.0-service.xml
-
-LOCAL_SRC_FILES := \
-    $(call all-c-files-under, .) \
-    $(call all-cpp-files-under, .)
-
-LOCAL_SHARED_LIBRARIES := \
-    libcutils \
-    liblog
-
-LOCAL_HEADER_LIBRARIES := \
-    libhardware_headers \
-    libhardware_legacy_headers
-
-LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)/include \
-    $(LOCAL_PATH)/gki/ulinux \
-    $(LOCAL_PATH)/gki/common \
-    $(LOCAL_PATH)/hal/include \
-    $(LOCAL_PATH)/hal/int \
-    $(LOCAL_PATH)/udrv/include
-
-LOCAL_CFLAGS := \
-    $(D_CFLAGS) \
-    -DNFC_HAL_TARGET=TRUE \
-    -DNFC_RW_ONLY=TRUE
-
-include $(BUILD_SHARED_LIBRARY)
+include $(call first-makefiles-under,$(call my-dir))
--- nfc/bcm2079x/product.mk
+++ nfc/bcm2079x/product.mk
@@ -16,10 +16,9 @@
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-impl-bcm \
     android.hardware.nfc@1.0-service \
     nfc_nci.bcm2079x.default \
-    nfc_nci.msm8974 \
     com.android.nfc_extras \
     NfcNci \
     Tag
@@ -28,6 +27,7 @@ PRODUCT_COPY_FILES += \
     device/samsung/msm8974-common/nfc/bcm2079x/nfcee_access.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/nfcee_access.xml \
     device/samsung/msm8974-common/nfc/bcm2079x/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     device/samsung/msm8974-common/nfc/bcm2079x/libnfc-brcm-20791b04.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm-20791b04.conf \
+    device/samsung/msm8974-common/nfc/bcm2079x/libnfc-brcm-20791b05.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm-20791b05.conf \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
