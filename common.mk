cc2055d501e87713790c7388e27dd1a7f839b2d4
Khalvat-M <mmba313@gmail.com>
2022-09-13 17:49:02 +0430

    msm8974-common: Use Aidl drm clearkey

--- common.mk
+++ common.mk
@@ -104,7 +104,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # FastCharge
 PRODUCT_PACKAGES += \
