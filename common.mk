fd1cbb30c0f6d30dda9956910708a449a28a311c
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-11-16 10:11:37 +0100

    msm8974-common: build wifi.supplicant/hostapd V1-ndk for macloader

--- common.mk
+++ common.mk
@@ -309,6 +309,8 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service.legacy \
+    android.hardware.wifi.hostapd-V1-ndk \
+    android.hardware.wifi.supplicant-V1-ndk \
     hostapd \
     libwpa_client \
     macloader \
